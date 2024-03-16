a3e657900c2dfb95c5a6a7c3b08144f79d0004c6
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-03-17 00:04:44 +0100

    add spring postgres tester

---
+++ spring-postgres-tester/src/main/java/com/example/springpostgrestester/SpringPostgresTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpostgrestester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgresTesterApplication.class, args);
+    }
+
+}
