51e1f16c319c60b9e9391b60bb43dcd337d1d9a6
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-04-03 21:54:39 +0200

    add impl for spring rabbitmq tester

---
+++ spring-rabbitmq-tester/src/main/java/com/example/springrabbitmqtester/Receiver.java
@@ -0,0 +1,21 @@
+package com.example.springrabbitmqtester;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+@Component
+public class Receiver {
+
+    private BlockingQueue<String> messages = new ArrayBlockingQueue<>(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        messages.add(message);
+    }
+
+    public BlockingQueue<String> getMessages() {
+        return messages;
+    }
+}
