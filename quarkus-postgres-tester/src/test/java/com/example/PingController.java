78e1cad89cfdd1290b84e71e52f3fa5e24d04c90
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-04-05 00:15:39 +0200

    add builds for testers

---
+++ quarkus-postgres-tester/src/test/java/com/example/PingController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import io.agroal.api.AgroalDataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.Map;
+
+@ApplicationScoped
+public class PingController {
+
+    @Inject
+    AgroalDataSource defaultDataSource;
+
+    @GET
+    @Path("/ping")
+    public Map<String, Object> ping() throws Exception {
+        Connection connection = defaultDataSource.getConnection();
+        connection.createStatement().execute("CREATE TABLE IF NOT EXISTS test (id SERIAL PRIMARY KEY, name TEXT)");
+        connection.createStatement().execute("insert into test (name) values ('test')");
+        Statement statement = connection.createStatement();
+        boolean next = statement.executeQuery("SELECT * FROM test where name = 'test'").next();
+        return Map.of("postgres", next);
+    }
+
+}
