c259c945ce417af59d09a1a692da16d86f856875
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-04-05 17:42:34 +0200

    setup mysql tester

--- spring-mysql-tester/src/main/java/com/example/springmysqltester/SpringMysqlTesterApplication.java
+++ spring-mysql-tester/src/main/java/com/example/springmysqltester/SpringMysqlTesterApplication.java
@@ -1,13 +1,36 @@
 package com.example.springmysqltester;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringMysqlTesterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMysqlTesterApplication.class, args);
+	}
+
+	@RestController
+	public class PingController {
+
+		@Autowired
+		DataSource ds;
+
+
+
+		@GetMapping("/ping")
+		public Map<String, Object> hello() throws Exception {
+			boolean execute = ds.getConnection().prepareStatement("SELECT 1").execute();
+			return Map.of("mysql", execute);
+		}
 	}
 
 }
