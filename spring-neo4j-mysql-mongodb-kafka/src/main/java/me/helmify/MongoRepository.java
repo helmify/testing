869a10d87c157232f2f206f1cadf70c798d02d10
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-04-28 00:18:50 +0200

    add spring test for kafka / mongodb / mysql / neo4j connectivity

---
+++ spring-neo4j-mysql-mongodb-kafka/src/main/java/me/helmify/MongoRepository.java
@@ -0,0 +1,4 @@
+package me.helmify;
+
+public interface MongoRepository extends org.springframework.data.mongodb.repository.MongoRepository<MongoEntity, String> {
+}
--- spring-neo4j-mysql-mongodb-kafka/src/main/java/me/helmify/SpringNeo4jMysqlMongodbKafkaApplication.java
+++ spring-neo4j-mysql-mongodb-kafka/src/main/java/me/helmify/SpringNeo4jMysqlMongodbKafkaApplication.java
@@ -3,13 +3,21 @@ package me.helmify;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static me.helmify.KafkaTopicConfig.KAKFA_TOPIC;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
+@EnableMongoRepositories
 public class SpringNeo4jMysqlMongodbKafkaApplication {
 
 	public static void main(String[] args) {
@@ -31,27 +39,47 @@ public class SpringNeo4jMysqlMongodbKafkaApplication {
 	@RestController
 	public class Neo4jPingController {
 
+		@Autowired
+		NeoRepository repo;
+
 		@GetMapping("/ping/neo4j")
 		public Map<String, Object> hello() throws Exception {
-			return Map.of("neo4j", false);
+			NeoEntity entity = new NeoEntity();
+			String name = "test";
+			entity.setName(name);
+			repo.save(entity);
+			return Map.of("neo4j", name.equals(repo.findAll().iterator().next().getName()));
 		}
 	}
 
 	@RestController
 	public class MongoDbPingController {
 
+		@Autowired
+		MongoRepository repo;
+
 		@GetMapping("/ping/mongodb")
 		public Map<String, Object> hello() throws Exception {
-			return Map.of("mongodb", false);
+			MongoEntity entity = new MongoEntity();
+			String name = "test";
+			entity.setName(name);
+			repo.save(entity);
+			return Map.of("mongodb", name.equals(repo.findAll().iterator().next().getName()));
 		}
 	}
 
 	@RestController
 	public class KafkaPingController {
 
+		@Autowired
+		private KafkaTemplate<String, String> kafkaTemplate;
+
 		@GetMapping("/ping/kafka")
 		public Map<String, Object> hello() throws Exception {
-			return Map.of("kafka", false);
+			String test = "kafka";
+			kafkaTemplate.send(KAKFA_TOPIC, test);
+			String poll = KafkaConsumer.messages.poll(10, TimeUnit.SECONDS);
+			return Map.of("kafka", test.equals(poll));
 		}
 	}
 }
