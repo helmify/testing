cd2f9ebea46f72be023dba75cbdc71b9d9d74361
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-05-15 20:21:25 +0200

    renames for consistency and avoiding issues with cluster object names longer 63 chars

--- spring-neo4j-mysql-mongodb-kafka/src/main/java/me/helmify/MongoRepository.java
+++
@@ -1,4 +0,0 @@
-package me.helmify;
-
-public interface MongoRepository extends org.springframework.data.mongodb.repository.MongoRepository<MongoEntity, String> {
-}
--- spring-neo4j-mysql-mongodb-kafka/src/main/java/me/helmify/NeoRepository.java
+++
@@ -1,6 +0,0 @@
-package me.helmify;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface NeoRepository extends Neo4jRepository<NeoEntity, Long> {
-}
